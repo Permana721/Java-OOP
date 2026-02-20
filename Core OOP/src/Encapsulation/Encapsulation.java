2e067cc19743b2b02bab34e6d6a26a2848333693
Permana <sdharma721@gmail.com>
2026-02-20 14:25:22 +0700

    Create Encapsulation.java

---
+++ Core OOP/src/Encapsulation/Encapsulation.java
@@ -0,0 +1,10 @@
+package Encapsulation;
+
+public class Encapsulation {
+    /**
+     * Encapsulation artinya memastikan data sensitif sebuah object tersembunyi dari akses luar
+     * Hal ini bertujuan agar kita bisa menjaga agar data sebuah object tetap baik dan valid
+     * Untuk mencapai ini, biasanya kita akan membuat semua field menggunakan access modifier private, sehingga tidak bisa diakses atau diubah dari luar
+     * Agar bisa diubah, kita akan menyediakan method untuk mengubah dan mendapatkan field tersebut
+     */
+}
