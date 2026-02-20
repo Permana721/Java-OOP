fac16ed7e166ea81ec58eabe000ccb9185843459
Permana <sdharma721@gmail.com>
2026-02-20 14:06:29 +0700

    update

---
+++ Core OOP/src/Encapsulation/AccessModifier/ProductApp.java
@@ -0,0 +1,12 @@
+package Encapsulation.AccessModifier;
+
+public class ProductApp {
+    static void main(String[] args) {
+        Product product = new Product();
+        product.name = "Indomie"; // Bisa di akses karena berada di dalam satu package yang sama
+        product.price = 3100; // Bisa di akses karena berada di dalam satu package yang sama
+
+        System.out.println(product.getName());
+        System.out.println(product.getPrice());
+    }
+}
