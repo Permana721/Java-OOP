d85089795a49156abaf82f06df3c73cc9d64c8f5
Permana <sdharma721@gmail.com>
2026-02-22 21:32:10 +0700

    update Person class comment for clarity
    Signed-off-by: Permana <sdharma721@gmail.com>

--- _02_Class/Person.java
+++ _02_Class/Person.java
@@ -3,6 +3,6 @@ package _02_Class;
 Untuk membuat class, kita bisa menggunakan kata kunci class
 Penamaan class biasa menggunakan format CamelCase
  */
-public class Person { // Ini adalah contoh class
+public class Person { // Ini adalah contoh class nya
 // class Person (Blueprint)
 }
