50537f27aa954bd4946505fe347a568639b7bacb
Permana <sdharma721@gmail.com>
2026-02-24 21:57:11 +0700

    Add main method to NestedApp for demonstrating inner class usage
    Signed-off-by: Permana <sdharma721@gmail.com>

--- _32_InnerClass/NestedApp.java
+++ _32_InnerClass/NestedApp.java
@@ -1,4 +1,15 @@
 package _32_InnerClass;
 
 public class NestedApp {
+    static void main(String[] args) {
+
+        Company company = new Company();
+        company.setName("Ipuh Corp");
+
+        Company.Employee employee =  company.new Employee();
+        employee.setName("Permana");
+
+        System.out.println("Company Name : " + employee.getCompany());
+        System.out.println("Employee Name : " + employee.getName());
+    }
 }
