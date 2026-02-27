959d64561b886be96e6a5d25f53a0a2b4976b0fe
Permana <sdharma721@gmail.com>
2026-02-27 20:50:35 +0700

    Add Constant class to demonstrate static fields and their usage
    Signed-off-by: Permana <sdharma721@gmail.com>

---
+++ _34_StaticKeyword/Country.java
@@ -0,0 +1,21 @@
+package _34_StaticKeyword;
+
+public class Country {
+    private String name;
+
+    public static class City { // Contoh static inner class
+        private String name;
+
+        public City() {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String cirebon) {
+
+        }
+    }
+}
