b27b50ac782146c3bc598a39d392977035bc4e0d
Permana <sdharma721@gmail.com>
2026-02-27 21:38:04 +0700

    Add BlankException class for handling blank input errors
    Signed-off-by: Permana <sdharma721@gmail.com>

---
+++ _38_RuntimeException/BlankException.java
@@ -0,0 +1,8 @@
+package _38_RuntimeException;
+
+public class BlankException extends RuntimeException { // Membuat Runtime Exception dengan extends RuntimeException
+    public BlankException(String message) {
+        super(message);
+    }
+}
+
