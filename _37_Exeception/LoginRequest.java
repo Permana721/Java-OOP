f9d524f117c5a1ce9a85c148a3d2f4496736b1c8
Permana <sdharma721@gmail.com>
2026-02-27 20:50:45 +0700

    Add LoginRequest record to handle user login data
    Signed-off-by: Permana <sdharma721@gmail.com>

---
+++ _37_Exeception/LoginRequest.java
@@ -0,0 +1,15 @@
+package _37_Exeception;
+
+public record LoginRequest(String username, String password) {
+    public LoginRequest {
+        System.out.println("Membuat object login request");
+    }
+
+    public LoginRequest(String username) {
+        this(username, "");
+    }
+
+    public LoginRequest() {
+        this("", "");
+    }
+}
