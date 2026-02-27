1ee76d11eee16ddad4675143be265fa69a911149
Permana <sdharma721@gmail.com>
2026-02-27 21:38:00 +0700

    Add comments to BlankException class explaining exception handling in Java
    Signed-off-by: Permana <sdharma721@gmail.com>

--- _37_Exeception/BlankException.java
+++ _37_Exeception/BlankException.java
@@ -1,5 +1,10 @@
 package _37_Exeception;
-
+/*
+Saat kita membuat aplikasi, kita tidak akan terhindar dengan yang namanya error
+Di Java, error direpresentasikan dengan istilah exception, dan semua direpresentasikan dalam bentuk class exception
+Kita bisa menggunakan class exception sendiri, atau menggunakan yang sudah disediakan oleh Java
+Jika kita ingin membuat exception, maka kita harus membuat class yang extends class Throwable atau turunan-turunannya
+ */
 public class BlankException extends RuntimeException {
     public BlankException(String message) {
         super(message);
