b356bb72e3fca42971eb444c70c85552facb63fb
Permana <sdharma721@gmail.com>
2026-03-01 19:26:21 +0700

    Add interface Borrowable
    Signed-off-by: Permana <sdharma721@gmail.com>

--- ManajemenPerpustakaan/Book.java
+++ ManajemenPerpustakaan/Book.java
@@ -1,4 +1,49 @@
 package ManajemenPerpustakaan;
 
 public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    // Constructor
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false; // default
+    }
+
+    public String getId(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public boolean isBorrowed(){
+        return isBorrowed;
+    }
+
+    // Method untuk meminjam buku
+    public void borrow() {
+        if (!isBorrowed) {
+            isBorrowed = true;
+            System.out.println("Book borrowed successfully.");
+        } else {
+            System.out.println("Book is already borrowed.");
+        }
+    }
+
+    // Method untuk mengembalikan buku
+    public void returnBook() {
+        if (isBorrowed) {
+            isBorrowed = false;
+            System.out.println("Book returned successfully.");
+        } else {
+            System.out.println("Book was not borrowed.");
+        }
+    }
 }
