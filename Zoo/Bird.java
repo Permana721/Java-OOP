3044d78048815dc4bfc27c3cf1fba4565b845c7a
Permana <sdharma721@gmail.com>
2026-03-04 19:43:29 +0700

    fix(bird): extend Bird from Animals, add makeSound method

--- Zoo/Bird.java
+++ Zoo/Bird.java
@@ -1,4 +1,11 @@
 package Zoo;
 
-public class Bird {
+public class Bird extends Animals{
+    public Bird(String name, int age){
+        super(name, age);
+    }
+
+    public void makeSound(){
+        System.out.println("Tweet!");
+    }
 }
