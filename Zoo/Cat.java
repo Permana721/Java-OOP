ce303449e735564ebf628006a786b70820f82e78
Permana <sdharma721@gmail.com>
2026-03-04 19:43:34 +0700

    "refactor(Cat): extend Animals class, add constructor and makeSound method"

--- Zoo/Cat.java
+++ Zoo/Cat.java
@@ -1,4 +1,11 @@
 package Zoo;
 
-public class Cat {
+public class Cat extends Animals {
+    public Cat(String name, int age){
+        super(name, age);
+    }
+
+    public void makeSound(){
+        System.out.println("Meow!");
+    }
 }
