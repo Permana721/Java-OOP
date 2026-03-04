088caa7bb213b15a32177a06435b2dcd233c2dec
Permana <sdharma721@gmail.com>
2026-03-04 19:43:45 +0700

    feat(Zoo): add main method with animal list and display info/sound

--- Zoo/Main.java
+++ Zoo/Main.java
@@ -1,4 +1,20 @@
 package Zoo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    static void main(String[] args) {
+        List<Animals> animals = new ArrayList<>();
+
+        animals.add(new Dog("Doggy", 3));
+        animals.add(new Cat("kitty", 2));
+        animals.add(new Bird("Rio", 1));
+
+        for(Animals animal : animals){
+            animal.getInfo();
+            animal.makeSound();
+            System.out.println("------------------");
+        }
+    }
 }
