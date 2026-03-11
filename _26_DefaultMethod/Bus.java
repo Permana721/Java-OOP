93fbce8c1a185c76473d42013b697e0f594f499c
Permana <sdharma721@gmail.com>
2026-03-11 20:06:23 +0700

    refactor(Bus): Introduce Car interface, add drive(), getTire(), getBrand(), isMaintenance(), and isBig() methods

--- _26_DefaultMethod/Bus.java
+++ _26_DefaultMethod/Bus.java
@@ -1,4 +1,24 @@
 package _26_DefaultMethod;
 
-public class Bus {
+public class Bus implements Car{
+
+    public void drive() {
+        System.out.println("Bus drive");
+    }
+
+    public int getTire() {
+        return 8;
+    }
+
+    public String getBrand() {
+        return "Hino";
+    }
+
+    public boolean isMaintenance() {
+        return false;
+    }
+
+    public boolean isBig(){
+        return true;
+    }
 }
