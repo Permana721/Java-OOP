7ed2310e8afc5ea1516147e1e711df8f4e677a8d
Permana <sdharma721@gmail.com>
2026-03-11 20:06:19 +0700

    feat(interface): Extend HasBrand in Car interface

--- _25_InterfaceInheritance/Car.java
+++ _25_InterfaceInheritance/Car.java
@@ -4,7 +4,7 @@ Sebelumnya kita sudah tahu kalo di Java, child class hanya bisa punya 1 class pa
 Namun berbeda dengan interface, sebuah child class bisa implement lebih dari 1 interface
 Bahkan interface pun bisa implement interface lain, bisa lebih dari 1. Namun jika interface ingin mewarisi interface lain, kita menggunakan kata kunci extends, bukan implements
  */
-public interface Car {
+public interface Car extends HasBrand {
     void drive();
 
     int getTire();
