6b1e7b785367aa2db264aa6e984d797b899ce37a
Permana <sdharma721@gmail.com>
2026-03-11 20:06:42 +0700

    `refactor(ProductApp): introduce main method, print object using toString`

--- _27_ToStringMethod/ProductApp.java
+++ _27_ToStringMethod/ProductApp.java
@@ -1,4 +1,12 @@
 package _27_ToStringMethod;
 
 public class ProductApp {
+    static void main(String[] args) {
+        Product product = new Product("Pongo 725", 9_000_000);
+
+        System.out.println(product.name);
+        System.out.println(product.price);
+
+        System.out.println(product);
+    }
 }
