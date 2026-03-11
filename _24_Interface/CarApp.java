a37348b7774459f9eb98cdcf573448e53c01f871
Permana <sdharma721@gmail.com>
2026-03-11 20:06:16 +0700

    feat(CarApp): introduce Avanza class and main method for demonstration

--- _24_Interface/CarApp.java
+++ _24_Interface/CarApp.java
@@ -1,4 +1,9 @@
 package _24_Interface;
 
 public class CarApp {
+    static void main(String[] args) {
+        Car car = new Avanza();
+        System.out.println(car.getTire());
+        car.drive();
+    }
 }
